ce4dbb9e87da6a1a124107590d5ae1e043d72b0d
LuisMm16 <lemartinezm@unitru.edu.pe>
2022-01-12 16:55:06 -0500

    Ejercicio 1

---
+++ src/com/bootcamp/Principal.java
@@ -0,0 +1,28 @@
+package com.bootcamp;
+
+public class Principal {
+    public static void main(String[] args) {
+        //Vamo a imprimir datos
+        byte numero1 = 127;
+        short numero2 = 32767;
+        int numero3 = 500012;
+        long numero4 = 521515432;
+        float numeroDecimal1 = 456.1f;
+        double numeroDecimal2 = 1215.145;
+        boolean verdadero = true;
+        boolean falso = false;
+        char caracter = 'a';
+        String palabra = "Monitor";
+        System.out.println("byte: " + numero1);
+        System.out.println("short: " + numero2);
+        System.out.println("int: " + numero3);
+        System.out.println("long: " + numero4);
+        System.out.println("float: " + numeroDecimal1);
+        System.out.println("double: " + numeroDecimal2);
+        System.out.println("boolean: " + verdadero);
+        System.out.println("boolean: " + falso);
+        System.out.println("char: " + caracter);
+        System.out.println("String: " + palabra);
+
+    }
+}
